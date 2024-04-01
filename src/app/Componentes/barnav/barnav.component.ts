184732c6bde94cee010f1be20da6b22759f3124a
NachoMezaM <nachom1605@gmail.com>
2024-04-01 14:41:02 -0300

    RouterLink

--- src/app/Componentes/barnav/barnav.component.ts
+++ src/app/Componentes/barnav/barnav.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-barnav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './barnav.component.html',
   styleUrl: './barnav.component.css'
 })
