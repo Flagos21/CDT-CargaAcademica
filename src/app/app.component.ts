bd51a9220850f6443ba41c05d5553cebee5fe8fc
Benyiman <89081846+Benyiman@users.noreply.github.com>
2024-04-01 11:14:02 -0300

    Avance 1/04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {  RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [RouterOutlet]
 })
 
 export class AppComponent {
